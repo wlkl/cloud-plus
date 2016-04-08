d3b06141ebea69bec063feb12b06cda6a8240556
wlkl <v.klyukvin@gmail.com>
2016-04-08 15:26:32 +0300

    start project

---
+++ cloud.py
@@ -0,0 +1,12 @@
+__author__ = 'wlkl'
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
