ee1b091a08030805e81aa8a50c2b94424cb96184
wlkl <v.klyukvin@gmail.com>
2016-04-11 17:27:54 +0300

    add templates and style

--- cloud.py
+++ cloud.py
@@ -1,12 +1,12 @@
 __author__ = 'wlkl'
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello</h1>'
+    return render_template('layout.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
